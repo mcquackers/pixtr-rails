a76fe248249cbc1ad824d754d824568c157382eb
Brian McQueen <brian.d.mcqueen@gmail.com>
2014-10-02 10:02:54 -0400

    Monban integration

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
 
+  resource :session, only: [:new, :create, :destroy]
   root "galleries#index"
-
+  resources :users, only: [:new, :create]
   resources :galleries do
     resources :images, only: [:show, :new, :create, :edit, :update, :destroy]
   end
